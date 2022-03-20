aad24fcc20f1bf3f988d12118284e7cf5472a401
Luis Eduardo Cislaghi <luis.cislaghi@gmail.com>
2022-03-20 20:46:12 -0300

    feat: update react native and gradle

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import Outdoor from './src';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <Outdoor />;
+  return (
+    <NavigationContainer>
+      <StatusBar hidden backgroundColor="transparent" />
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={Outdoor} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
